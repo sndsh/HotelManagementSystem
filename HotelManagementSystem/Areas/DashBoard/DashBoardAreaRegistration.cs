234b1ede6f240afed9eef2f337c2f29e0d187cb1
sandesh <sndshkrk@gmail.com>
2020-01-12 17:32:21 +0545

    admin pannel and logoff initiated

---
+++ HotelManagementSystem/Areas/DashBoard/DashBoardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HotelManagementSystem.Areas.DashBoard
+{
+    public class DashBoardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashBoard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashBoard_default",
+                "DashBoard/{controller}/{action}/{id}",
+                new {controller="Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
